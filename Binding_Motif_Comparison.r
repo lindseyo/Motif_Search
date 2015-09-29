8bb12a36bf518f97f5fc6dbc8120342b196e8735
lindseyo <losimiri@gmail.com>
2015-09-28 23:56:21 -0700

    changed algorithm to compare kl divergence scores instead of direct overlap

--- Binding_Motif_Comparison.r
+++ Binding_Motif_Comparison.r
@@ -1,4 +1,5 @@
 library(readr)
+library(entropy)
 recSiteSubStr <- function(consSite1, startNuc, stopNuc){
   #subset the given area of the consensus recognition site, used to find the largest string
   #without N's
@@ -19,78 +20,71 @@ maxSiteDist <- function(siteVec, consSite){
   return(c(maxDistance, recSite))
 }
 
+##distance formula helper function
+distForm <- function(distancesVec){
+  distsSq <- distancesVec^2
+  return(sqrt(sum(distsSq)))
+}
 
 pfm <- read_csv("pfm_fungi.csv", col_names = FALSE)
 orf1000 <- read_csv("orf_genomic_1000.csv", col_names = FALSE)
 TFs <- list()
-consSeqs <- list()
+posWeightMatList <- list()
+#consSeqs <- list()
 nucOrder <- c("A", "C", "G", "T")
-consRatio <- .6
-minConsSeqLen <- 6
+#consRatio <- .6
+#minConsSeqLen <- 6
 for (rowIndex in 1:nrow(pfm)){
   if (grepl(">", pfm[rowIndex, 1])){
     TFs[length(TFs) + 1] <- pfm[rowIndex, 1]
-    consSeq <- ""
-    for (colIndex in 1:ncol(pfm)){
-      #lapply?? can this be vectorized rather easily?
-      for (nucIndex in 1:4){
-        if (sum(as.integer(pfm[(rowIndex+1):(rowIndex+4), colIndex])) == 0){
-          break()
-        }else if ((as.integer(pfm[(rowIndex + nucIndex), colIndex])/sum(as.integer(pfm[(rowIndex+1):(rowIndex+4), colIndex]))) > consRatio){
-          consSeq <- paste0(consSeq, nucOrder[nucIndex])
-          break()
-        }else if (nucIndex == 4){
-          consSeq <- paste0(consSeq, "N")
-          }
-        }
-    }
-    consSeqs[length(consSeqs) + 1] <- consSeq
-    }
-}
-maxRecSites <- list()
-strongRecSites <- list()
-strongTFIDs <- list()
-
-for (recSiteIndex in 1:length(consSeqs)){
-  recSiteVec <- unlist(strsplit(consSeqs[[recSiteIndex]][1], ""), recursive = FALSE)
-  nonspecSites <- which(recSiteVec == "N")
-  if (length(nonspecSites) == 0){
-    strongRecSites[length(strongRecSites) + 1] <- consSeqs[[recSiteIndex]][1]
-    strongTFIDs[length(strongTFIDs) + 1] <- TFs[[recSiteIndex]][1]
-    next()
+    posWeightMat <- data.matrix(pfm[(rowIndex+1):(rowIndex+4),])
+    emptyCols <- which(apply(posWeightMat, 2, sum)==0)
+    if (length(emptyCols) > 0){
+      posWeightMatTrunc <- posWeightMat[, 1:(emptyCols[1]-1)]
+    }else{posWeightMatTrunc <- posWeightMat}
+    posWeightMatList[[length(posWeightMatList)+1]] <- posWeightMatTrunc
   }
-  maxSiteDistRes <- maxSiteDist(nonspecSites, consSeqs[[recSiteIndex]][1])
-  maxSpecificity <- maxSiteDistRes[1]
-  maxRecSite <- maxSiteDistRes[2]
-  if (maxSpecificity >= minConsSeqLen){
-    strongRecSites[length(strongRecSites) + 1] <- consSeqs[[recSiteIndex]][1]
-    maxRecSites[length(maxRecSites) + 1] <- maxRecSite
-    strongTFIDs[length(strongTFIDs) + 1] <- TFs[[recSiteIndex]][1]
-  }
-}
+}  
 
 
-#match to genome
-#vectorize???
-possibleTFs <- list()
+#create genome freq matrices
+#possibleTFs <- list()
+promoterMatList <- list()
 geneIDs <- list()
 for (geneORFInd in seq_len(nrow(orf1000))){
   if (charmatch(">", orf1000[geneORFInd,1], nomatch = FALSE)){
-    promoter <- paste0(orf1000[(geneORFInd+1):(geneORFInd + 14), 1], collapse = "")
+    promoter <- paste0(orf1000[(geneORFInd+1):(geneORFInd + 18), 1], collapse = "")
+    promoter <- substr(promoter, 1, 1001)
+    promoterNucIndices <- as.numeric(sapply(strsplit(promoter, split = ""), match, nucOrder))
     geneIDs[length(geneIDs) + 1] <- orf1000[geneORFInd,1]
-    geneTFs <- list()
-    for (strongRecSiteInd in seq_len(length(maxRecSites))){
-      if (charmatch(maxRecSites[[strongRecSiteInd]][1], promoter, nomatch = FALSE)){
-        geneTFs[length(geneTFs) + 1] <- strongTFIDs[strongRecSiteInd]
-      }
+    promoterMat <- matrix(data = 0, nrow = 4, ncol = length(promoterNucIndices))
+    #vectorize???
+    for (nucInd in seq_len(length(promoterNucIndices))){
+      promoterMat[promoterNucIndices[nucInd], nucInd] <- 1
     }
-    if (length(geneTFs) > 0){
-      print(geneTFs)
-      possibleTFs[length(possibleTFs) + 1] <- geneTFs
-    }else{possibleTFs[length(possibleTFs) + 1] <- list(NA)}
-    
+    promoterMatList[[length(promoterMatList) + 1]] <- promoterMat
   }
 }
 
-genesWithTFs <- possibleTFs[which(!is.na(possibleTFs))]
-genesWithTFsIDs <- geneIDs[which(!is.na(possibleTFs))]
+start.time <- proc.time()
+#multicore
+#calc KL divergences using sliding windows
+allKLDists <- list()
+for (promoterMatInd in seq_len(length(promoterMatList))){
+  promoterMat1 <- promoterMatList[[promoterMatInd]]
+  promMinKLDistList <- list()
+  ##parallelize at tf leveltime
+  for (tfpwmInd in seq_len(length(posWeightMatList))){
+    tfpwm <- posWeightMatList[[tfpwmInd]]
+    recSiteLen <- ncol(tfpwm)
+    #use parlapply, don't need 2d apply
+    #does not give leniency for pyrimidines or whatever
+    klDists <- lapply(seq_len((ncol(promoterMat1)-recSiteLen)), function(nucInd1) 
+      distForm(mapply(function(x,y) 
+        KL.plugin(promoterMat[,x], tfpwm[,y]), seq(from=nucInd1, to=(nucInd1 + recSiteLen)), seq_len(recSiteLen))))
+    promMinKLDistList[[tfpwmInd]] <- min(unlist(klDists, recursive = FALSE))
+  }
+  allKLDists[[promoterMatInd]] <- promMinKLDistList
+}
+
+time.elapsed <- proc.time() - start.time
